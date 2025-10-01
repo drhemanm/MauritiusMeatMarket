973c01a248fb4fd25e8c3bbaa8900813fb77a5cb
drhemanm <heman.m@iafrica.solutions>
2025-10-01 13:34:22 +0400

    Create layout.tsx

---
+++ app/customers/layout.tsx
@@ -0,0 +1,15 @@
+/**
+ * Customers Layout
+ * 
+ * Uses the main dashboard layout
+ * 
+ * @module app/customers/layout
+ */
+
+export default function CustomersLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
