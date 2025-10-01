f51f21d61666df76e7a3d27f14511326a71605fa
drhemanm <heman.m@iafrica.solutions>
2025-10-01 11:57:46 +0400

    Create index.ts

---
+++ components/layout/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Layout Components Exports
+ * 
+ * Central export point for all layout components.
+ * 
+ * @module components/layout
+ */
+
+export { Sidebar } from './Sidebar';
+export { Header } from './Header';
+export { MainLayout } from './MainLayout';
