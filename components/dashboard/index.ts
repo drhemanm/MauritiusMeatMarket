782fa58efdd27e2aec61e9bb6f143537a4668dcb
drhemanm <heman.m@iafrica.solutions>
2025-10-01 12:03:57 +0400

    Create index.ts

---
+++ components/dashboard/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Dashboard Components Exports
+ * 
+ * @module components/dashboard
+ */
+
+export { KPICard } from './KPICard';
+export { SalesChart } from './SalesChart';
+export { SalesDistributionChart } from './SalesDistributionChart';
+export { RecentOrders } from './RecentOrders';
