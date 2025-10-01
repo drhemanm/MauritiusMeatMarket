55f0245bb4b4c05571b666c37de7edeaedbf842f
drhemanm <heman.m@iafrica.solutions>
2025-10-01 11:55:32 +0400

    Create index.ts

---
+++ components/ui/index.ts
@@ -0,0 +1,12 @@
+/**
+ * UI Components Exports
+ * 
+ * Central export point for all reusable UI components.
+ * 
+ * @module components/ui
+ */
+
+export { Button } from './Button';
+export { Card } from './Card';
+export { Input } from './Input';
+export { Badge } from './Badge';
